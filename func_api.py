cce0db3b5cb3952936d7f4a6b02e4a3a23241c51
bostonsam617 <samuel.brenner@boston.gov>
2026-02-14 15:59:59 -0500

    working api

---
+++ func_api.py
@@ -0,0 +1,42 @@
+import os
+import pathlib
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+from pydantic import BaseModel, Field
+from typing import List
+
+class Item(BaseModel):
+    item_name: str = Field(description="The name of the item.")
+    unit_price: int = Field(description="price of the item per unit in cents")
+    unit: str = Field(description="unit of the item")
+
+class Reciept(BaseModel):
+    items: list[Item]
+
+load_dotenv()
+
+API_KEY = os.getenv('KEY')
+client = genai.Client(
+    api_key=API_KEY)
+
+filepath = pathlib.Path('tj_reciepts/receipt.jpg').expanduser()
+
+prompt = "please extract all items and prices from this receipt"
+response = client.models.generate_content(
+  model="gemini-2.5-flash",
+  contents=[
+      types.Part.from_bytes(
+        data=filepath.read_bytes(),
+        mime_type='image/jpg',
+      ),
+    prompt
+  ],
+    config={
+        "response_mime_type": "application/json",
+        "response_json_schema": Reciept.model_json_schema(),
+    }
+)
+
+receipt = Reciept.model_validate_json(response.text)
+print(receipt)
---
+++ func_categorize.py
@@ -0,0 +1,16 @@
+import func_api
+
+items = func_api.receipt
+
+for item in range(items):
+    print(item)
+
+#print(items)
+# with open('input.csv', 'r') as infile:
+#     reader = csv.reader(infile)
+#     rows = list(reader)
+
+
+
+# a.category = input(f'What category is {a.item}? ')
+# print(a.category)
